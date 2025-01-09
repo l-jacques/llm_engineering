d7082fc0de76b5ee633bf93d9700157425f2dffa
Laurent JACQUES <laurent.p.jacques@gmail.com>
2025-01-09 09:35:24 +0100

    add week2 day3 exercise variation with dropdown and multisystem

--- week2/community-contributions/AISystem.py
+++ week2/community-contributions/AISystem.py
@@ -1,7 +1,41 @@
-
+import os
 from enum import Enum, auto
+from dotenv import load_dotenv
 from openai import OpenAI
 import anthropic
+
+def load_allbots():
+    load_dotenv()
+    openai_api_key = os.getenv('OPENAI_API_KEY')
+    anthropic_api_key = os.getenv('ANTHROPIC_API_KEY')
+    google_api_key = os.getenv('GOOGLE_API_KEY')
+    
+    if openai_api_key:
+        print(f"OpenAI API Key exists and begins {openai_api_key[:8]}")
+    else:
+        print("OpenAI API Key not set")
+        
+    if anthropic_api_key:
+        print(f"Anthropic API Key exists and begins {anthropic_api_key[:7]}")
+    else:
+        print("Anthropic API Key not set")
+    
+    if google_api_key:
+        print(f"Google API Key exists and begins {google_api_key[:8]}")
+    else:
+        print("Google API Key not set")
+    
+    openai = OpenAI()
+    
+    claude = anthropic.Anthropic()
+    
+    gemini_via_openai = OpenAI(
+        api_key=google_api_key, 
+        base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+    )
+    ollama_via_openai = OpenAI(base_url='http://localhost:11434/v1', api_key='ollama')
+
+    return openai, claude, gemini_via_openai, ollama_via_openai
 
 def formatPrompt(role, content):
     return {"role": role, "content": content}
